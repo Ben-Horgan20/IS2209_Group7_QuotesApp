1f54a5d8662091fd63eefcb9fb9f4d2b34b23a33
benhorgan <124378941@umail.ucc.ie>
2026-03-04 13:13:03 +0000

    plz work now

--- app.py
+++ app.py
@@ -1,12 +1,54 @@
-from flask import Flask
-
+from flask import Flask, render_template, request
+import requests
+from dotenv import load_dotenv
+import os
 app = Flask(__name__)
 
+load_dotenv()
+
+API_KEY = os.getenv("NAME_API_KEY")
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello colm!'
+def index():
+
+    # Quote of the Day
+    qotd_response = requests.get(
+        'https://api.api-ninjas.com/v2/quoteoftheday',
+        headers={"X-Api-Key": API_KEY}
+    )
+    data = qotd_response.json()[0]
+    qotd_quote = data['quote']
+    qotd_author = data['author']
+    qotd_work = data['work']
+
+    # Random quotes
+    rand_response = requests.get(
+        'https://api.api-ninjas.com/v2/randomquotes',
+        headers={"X-Api-Key": API_KEY}
+    )
+    data = rand_response.json()[0]
+    rand_quote = data['quote']
+    rand_author = data['author']
+    rand_work = data['work']
+
+    # Generic quotes
+    categories = 'success,wisdom'
+    response = requests.get(
+        'https://api.api-ninjas.com/v2/randomquotes',
+        headers={"X-Api-Key": API_KEY},
+        params={'categories': categories}
+    )
+    data = rand_response.json()[0]
+    quote = data['quote']
+    author = data['author']
+    work = data['work']
+
+    return render_template('index.html', qotd_quote=qotd_quote, qotd_author=qotd_author, qotd_work=qotd_work,
+                           rand_quote=rand_quote, rand_author=rand_author, rand_work=rand_work,
+                           quote=quote, author=author, work=work)
+
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
